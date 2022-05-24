78b8ccb18f74b65bbf156bbf400538c7db67d748
eldaniell01 <amontepequem@miumg.edu.gt>
2022-05-23 22:00:40 -0600

    validaciones path

--- hello/main.py
+++ hello/main.py
@@ -1,6 +1,7 @@
+from importlib.resources import path
 from typing import Optional
 from pydantic import BaseModel
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, Path
 from fastapi import Body
 
 
@@ -25,7 +26,15 @@ def create_persona(person: Person = Body(...)):
 
 @app.get('/person/detail')
 def show_person(
-    name: Optional[str] = Query(None, min_length=1, max_length=50),
-    age: int = Query(...)
+    name: Optional[str] = Query(None, min_length=1, max_length=50, title="Persona", description="nombre de la persona"),
+    age: int = Query(..., title="Edad", description="this is the person age")
 ):
     return {name: age}
+
+#validations 
+
+@app.get('/person/detail/{person_id}')
+def show_person(
+    person_id: int = Path(..., gt=0)
+): 
+    return {person_id: "existe"}
