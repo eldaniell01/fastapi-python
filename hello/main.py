4d101b429f9be9bd25fa7949fca2c0a1ee8bdb14
eldaniell01 <amontepequem@miumg.edu.gt>
2022-05-23 19:44:56 -0600

    modelos

--- hello/main.py
+++ hello/main.py
@@ -1,8 +1,22 @@
+from typing import Optional
+from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi import Body
 
 
 app = FastAPI()
 
+#models
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    age: int
+    color_pelo: Optional[str] = None
+
 @app.get('/')
 def home():
     return {"hello": "World"}
+
+@app.post('/person/new')
+def create_persona(person: Person = Body(...)):
+    return person
